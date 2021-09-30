7679d87fa6f56a7883926147ef80cb0020731d27
Thomas <thomaslio@live.no>
2021-09-30 16:07:18 +0200

    Changed from Promise to Observable on Main page

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { TableHeader } from 'src/app/interfaces/table-header';
+import { Observable, Subscription } from 'rxjs';
 
 
 interface User {
@@ -18,26 +19,27 @@ interface User {
 
 export class MainComponent implements OnInit {
 
+  subscription: Subscription;
   title: string;
   users: User[];
   tableHeaders: TableHeader[] = [
-    { field: 'name', header: 'Name', sort: false },
+    { field: 'name', header: 'Name', sort: true },
     { field: 'username', header: 'Username', sort: false },
     { field: 'email', header: 'Email', sort: false },
     { field: 'website', header: 'Website', sort: false }];
 
 
-  constructor(
-    private http: HttpClient
-  ) { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.getUsers().then((users: User[]) => {
-      this.users = users;
-    });
+    this.subscription = this.getUsers().subscribe((users: User[]) => { this.users = users });
   }
 
-  getUsers(): Promise<User[]> {
-    return this.http.get('https://jsonplaceholder.typicode.com/users').toPromise().then((data: User[]) => { return data });
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  private getUsers(): Observable<User[]> {
+    return this.http.get('https://jsonplaceholder.typicode.com/users') as Observable<User[]>;
   }
 }
